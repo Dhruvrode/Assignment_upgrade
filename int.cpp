1af98247f99282781bf8f0d097398bcb5ed82a32
Dhruv rode <89011471+Dhruvrode@users.noreply.github.com>
2023-02-23 21:43:08 +0530

    Add files via upload

---
+++ int.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+
+    cin>>a;
+    cout<<"Value of a =>"<<a<<endl;
+
+    cin>>b;
+    cout<<"Value of b =>"<<b<<endl;
+ 
+
+    return 0;
+}
